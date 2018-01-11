263d8b2e0e2f7544649658cd86fffa9780bcf916
inhere <in.798@qq.com>
2018-01-11 21:14:04 +0800

    remove some demo files and tool class to inher/console-components

--- tests/Components/files/text-1.tpl
+++
@@ -1,9 +0,0 @@
-{#include 'text-0.tpl' }
-
-date: {$date}
-I am in the text-1.tpl
-{*
-comments
-comments
-*}
-{#include "text-0.tpl "}
--- tests/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# phpunit6.phar --colors --coverage-html ./coverage/
-phpunit6.phar --colors --bootstrap tests/boot.php tests
